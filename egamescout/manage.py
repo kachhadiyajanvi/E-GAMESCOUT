1f9d9663da1b18f94a00070c0b1b6cc073fec87d
kachhadiyajanvi <janvikachhadiya1@gmail.com>
2026-02-03 15:34:40 +0530

    implement SQL 3

--- egamescout/manage.py
+++ egamescout/manage.py
@@ -2,11 +2,32 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import pymysql
+
+pymysql.install_as_MySQLdb()
+import MySQLdb
+# Monkey patch the version to satisfy Django's requirement
+if hasattr(MySQLdb, 'version_info'):
+    MySQLdb.version_info = (2, 2, 7, 'final', 0)
+else:
+    pymysql.version_info = (2, 2, 7, 'final', 0)
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'egamescout.settings')
+
+    # Bypass MariaDB version check for older XAMPP versions
+    try:
+        from django.db.backends.mysql.base import DatabaseWrapper
+        DatabaseWrapper.check_database_version_supported = lambda self: None
+        
+        # Disable RETURNING support for MariaDB < 10.5
+        from django.db.backends.mysql.features import DatabaseFeatures
+        DatabaseFeatures.can_return_columns_from_insert = False
+    except ImportError:
+        pass
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
