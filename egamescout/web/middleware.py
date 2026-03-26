98a49f2815e390962dc839af29ca3f4de2c298a5
kachhadiyajanvi <janvikachhadiya1@gmail.com>
2026-03-26 20:24:56 +0530

    Organization Player Accept issues

--- egamescout/web/middleware.py
+++ egamescout/web/middleware.py
@@ -14,6 +14,7 @@ class SecureSessionValidationMiddleware:
             '/organization/login/',
             '/organization/register/',
             '/organization/resend-otp/',
+            '/organization/player/accept-invite/',
             '/player/login/', # if applicable
         ]
 
